571e36eb2e003c6e0cb793caa83b3cf36a76246c
make-ing <bckr.florian@gmail.com>
2017-01-17 15:19:46 +0100

    added uninstall and install to update script.

--- update.sh
+++ update.sh
@@ -3,7 +3,9 @@
 printf "Downloading new version...\n"
 git fetch --all
 git reset --hard origin/master
-printf "finished!\n"
+printf "Remove old version...\n"
+sudo pip uninstall mrbeam_ledstrip
+printf "Installing new version...\n"
+sudo python setup.py install
 printf "Restarting service...\n"
 sudo systemctl restart mrbeam_ledstrip.service
-printf "finished!\n"
