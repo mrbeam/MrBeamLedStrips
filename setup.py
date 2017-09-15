ff437850b7df53b8d5b29136c058d289f2cf73ae
make-ing <bckr.florian@gmail.com>
2017-09-15 11:22:59 +0200

    changed version to 0.1.7

--- setup.py
+++ setup.py
@@ -154,7 +154,7 @@ def get_cmdclass():
 
 def params():
     name = "mrbeam_ledstrips"
-    version = '0.1.6'
+    version = '0.1.7'
     description = DESCRIPTION
     long_description = LONG_DESCRIPTION
     author = "Teja Philipp"
