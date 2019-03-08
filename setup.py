fbe575adb3a03176087ec9fad81acfbc5a95291c
Iratxe Espartza Chueca <iratxe@mr-beam.org>
2019-03-08 14:38:57 +0100

    v0.1.15

--- setup.py
+++ setup.py
@@ -154,7 +154,7 @@ def get_cmdclass():
 
 def params():
     name = "mrbeam_ledstrips"
-    version = '0.1.14'
+    version = '0.1.15'
     description = DESCRIPTION
     long_description = LONG_DESCRIPTION
     author = "Teja Philipp"
