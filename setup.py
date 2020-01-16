f74edbe6a1e8561ff15b92e97498a7fdfd49d9cc
Iratxe <irlaec@gmail.com>
2020-01-16 13:25:27 +0100

    v0.1.18 (#7)
    * shutdown improvements.
    
    * v0.1.18
    
    Co-authored-by: Teja <github@tejaphilipp.de>

--- setup.py
+++ setup.py
@@ -154,7 +154,7 @@ def get_cmdclass():
 
 def params():
     name = "mrbeam_ledstrips"
-    version = '0.1.17'
+    version = '0.1.18'
     description = DESCRIPTION
     long_description = LONG_DESCRIPTION
     author = "Teja Philipp"
