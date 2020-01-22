1b855a2bbd87f4b6908983307615735a267a5e3f
Andy Werner <andy@mr-beam.org>
2020-01-22 22:01:06 +0100

    0.1.19: Introducing set: command to set settings like brightness

--- setup.py
+++ setup.py
@@ -154,7 +154,7 @@ def get_cmdclass():
 
 def params():
     name = "mrbeam_ledstrips"
-    version = '0.1.18'
+    version = '0.1.19'
     description = DESCRIPTION
     long_description = LONG_DESCRIPTION
     author = "Teja Philipp"
