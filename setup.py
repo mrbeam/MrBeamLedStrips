8426e6ee49d842f74da5b5d09b492e342786f85a
Josef-MrBeam <81746291+Josef-MrBeam@users.noreply.github.com>
2021-07-15 16:29:31 +0200

    SW-153 bump version

--- setup.py
+++ setup.py
@@ -149,7 +149,7 @@ if sys.version_info >= (3,0):
 	install_requires += ["rpi-ws281x; platform_machine=='armv7l'",]
 setup(
 	name = "mrbeam_ledstrips",
-	version = '0.2.1',
+	version = '0.2.2',
 	description = DESCRIPTION,
 	long_description = LONG_DESCRIPTION,
 	author = "Teja Philipp",
